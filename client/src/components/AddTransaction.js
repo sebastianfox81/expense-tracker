b7cac973abb1fe1ecbbd6b3f6e2cfc3910e0f681
Sebastian <sebastianfox81@gmail.com>
2022-02-04 16:53:04 -0500

    Added change handlers to AddTransaction component

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddTransaction = () => {
+
+  const [ name, setName ] = useState('');
+  const [ amount, setAmount ] = useState(0);
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleAmount = (e) => {
+    setAmount(e.target.value)
+  }
+
   return <div>
     <h1>Add Transaction</h1>
+    <form>
+      <label htmlFor="text">Text</label>
+      <input id="name" type="text" onChange={handleChange}/>
+      <label htmlFor="amount">Amount</label>
+      <input type="number" onChange={handleAmount}/>
+    </form>
   </div>;
 };
 
