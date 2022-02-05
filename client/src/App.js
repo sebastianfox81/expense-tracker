a6cd0f49c1f4564e050c9dc2ab2f6455ec8ac4ee
Sebastian <sebastianfox81@gmail.com>
2022-02-05 14:15:56 -0500

    Implemented income expense calculation in IncomeExpenses component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import AppCss from './App.css';
+import './App.css';
 import React from 'react';
 import Header from './components/Header'
 import Balance from './components/Balance'
--- client/src/components/IncomeExpenses.js
+++ client/src/components/IncomeExpenses.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useGlobalContext } from './context'
 
 const IncomeExpenses = () => {
+
+  const { transactions } = useGlobalContext();
+  const amounts = transactions.map((item) => item.amount)
+
+  const income = amounts
+      .filter((amount) => amount > 0)
+      .reduce((acc, curr) => (acc += curr), 0).toFixed(2)
+  const expense = amounts
+      .filter(amount => amount < 0)
+      .reduce((acc, curr) => (acc += curr), 0).toFixed(2) * -1
+  console.log(expense)
+
   return <div>
-    
+
     <h4>Income</h4>
-    <p>+0.00</p>
+    <p>{income}</p>
     <h4>Expense</h4>
-    <p>-0.00</p>
+    <p>{expense}</p>
   </div>;
 };
 
 export default IncomeExpenses;
+
